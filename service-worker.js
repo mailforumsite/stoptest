a15a5870ffe6a66937efdfec0024039e26536c31
mailforumsite <i.vasilev75@proton.me>
2026-03-25 20:20:30 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,13 @@
-const CACHE_NAME = 'kat-pwa-v2';
+const CACHE_NAME = 'kat-pwa-v3';
+
 const ASSETS = [
   './',
   './index.html',
+  './advokati.html',
+  './dokumenti.html',
+  './drugtest.html',
   './lekarstva.html',
+  './speshno.html',
   './lekarstva.json',
   './manifest.json',
   './icon-192.png',
@@ -10,23 +15,33 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const clone = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-      return response;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      });
+    })
   );
 });
